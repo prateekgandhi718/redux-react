30cc27117d821f80ca1201af69804a97ea413201
Prateek Gandhi <Prateek.Gandhi@lntinfotech.com>
2023-02-20 13:19:48 +0530

    custom payload

--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ordered, restocked } from './iceCreamSlice'
 
 const IceCreamView = () => {
     const numOfIceCream = useSelector((state) => state.icecream.numOfIceCream)
     const dispatch = useDispatch()
+    const [num, setNum] = useState(1);
     
     const handleOrdering = () => {
         dispatch(ordered())
@@ -18,7 +19,12 @@ const IceCreamView = () => {
     <div>
       <h2>Number of icecreams - {numOfIceCream}</h2>
       <button onClick={() => handleOrdering()}>Order icecream</button>
-      <button onClick={() => handleRestocking(5)}>Restock icecreams</button>
+      <input 
+      type='number'
+      value={num}
+      onChange = {(e) => setNum(parseInt(e.target.value))}
+      />
+      <button onClick={() => handleRestocking(num)}>Restock icecreams</button>
     </div>
   )
 }
